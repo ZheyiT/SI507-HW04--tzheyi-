fccacfeb763e8d20606fe8510993c1b86084fb20
Zheyi Tian <tzheyi@umich.edu>
2018-01-30 17:26:29 -0500

    Debug

--- magic_eight.py
+++ magic_eight.py
@@ -5,9 +5,8 @@ if a == 'quit':
 while a[-1] is not '?':
 	a = input("I’m sorry, I can only answer questions. To quit, enter'quit'. \nPlease input your question again:")
 
-20_possible_answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", 'Cannot predict now', "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very Doubtful"]
-answer = randomrange(20_possible_answers)
-
+twenty_possible_answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", 'Cannot predict now', "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very Doubtful"]
+answer = random.choice(twenty_possible_answers)
 print(answer)
 
 

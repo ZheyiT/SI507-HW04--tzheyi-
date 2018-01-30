6928727fa9a5b2dafb41a395f4dc938effc90c60
Zheyi Tian <tzheyi@umich.edu>
2018-01-30 17:05:28 -0500

    A change the py file, adding exit function."
    quit

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,5 @@
 a = input("What is your question?")
+if a == 'quit':
+	quit()
+while a[-1] is not '?':
+	a = input("I’m sorry, I can only answer questions. To quit, enter'quit'. \nPlease input your question again:")

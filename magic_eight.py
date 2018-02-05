5afe9d9f6b0022deab5ea004c5de2c7aeaa87f73
Tongyan Xu <Tongyan@TongyandeMacBook-Pro.local>
2018-02-04 22:41:20 -0500

    add_questionsC

--- magic_eight.py
+++ magic_eight.py
@@ -1,14 +1,29 @@
+# -*- coding:utf-8 -*-
+
 import random
-a = input("What is your question?")
-if a == 'quit':
-	quit()
-while a[-1] is not '?':
-	a = input("I’m sorry, I can only answer questions. To quit, enter'quit'. \nPlease input your question again:")
-
-twenty_possible_answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", 'Cannot predict now', "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very Doubtful"]
-answer = random.choice(twenty_possible_answers)
-print(answer)
 
 
+def ask_question():
+    a = input("What is your question?")
+    if a == 'quit':
+        quit()
+    while a[-1] is not '?':
+        a = input("I’m sorry, I can only answer questions. To quit, enter 'quit'.\nPlease input your question again:")
+
+    twenty_possible_answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+                               "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes",
+                               "Signs point to yes", "Reply hazy try again", "Ask again later",
+                               "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+                               "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good",
+                               "Very Doubtful"]
+    answer = random.choice(twenty_possible_answers)
+    print(answer)
 
 
+def main():
+    while True:
+        ask_question()
+
+
+if __name__ == '__main__':
+    main()
